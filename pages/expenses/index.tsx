584c153c41ecb753af07e185c25c7ed15c882938
pc <pc@email.com>
2022-08-14 14:04:12 +0530

    Fix client side data fetching

--- pages/expenses/index.tsx
+++ pages/expenses/index.tsx
@@ -2,22 +2,35 @@ import { ExpensesList } from "../../components/ExpensesList";
 import { GetServerSideProps, NextPage } from "next";
 import { ExpenseProps } from "../../interfaces/types";
 import {Layout} from "../../components/Layout";
+import {useEffect, useState} from "react";
 
-const Expenses: NextPage<{ expenses: ExpenseProps[] }> = ({expenses}) => {
+const Expenses: NextPage = () => {
+    const [expenses, setExpenses] = useState<ExpenseProps[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetch('https://pro-expense-manager.herokuapp.com/expense/')
+            .then(res => res.json())
+            .then(data => {
+                setExpenses(data);
+                setIsLoading(false);
+            })
+    }, []);
+
+    if (isLoading) {
+        return <Layout>
+            <div className={"text-xl"}>
+                Loading...
+            </div>
+        </Layout>
+    }
+
     return (
         <Layout>
             <ExpensesList expenses={expenses} />
         </Layout>
     )
-}
-
-export const getServerSideProps: GetServerSideProps = async () => {
-    const res = await fetch('https://pro-expense-manager.herokuapp.com/expense/');
-    return {
-        props: {
-            expenses: await res.json()
-        }
-    }
 }
 
 export default Expenses;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,42 +5,25 @@ import {ExpenseProps} from "../interfaces/types";
 import {useEffect, useState} from "react";
 
 const Home: NextPage = () => {
-    const [expenses, setExpenses] = useState<ExpenseProps[]>([
-        {
-            id: '1',
-            title: 'title',
-            amount: 1,
-            category: 'category',
-            subcategory: 'subcategory',
-            time: 'time',
-            payee: 'payee',
-            currency: 'currency',
-            description: 'description',
-            rating: 1,
-        }
-    ]);
-    const [error, setError] = useState<string>();
-
-    async function fetchRecentExpenses() {
-        // fetch('https://pro-expense-manager.herokuapp.com/expense/recent/')
-        //     .then(res => res.json())
-        //     .then(data => {
-        //         setExpenses(data);
-        //     })
-        //     .catch(err => {
-        //         setError(err.message);
-        //     })
-        const res = await fetch('https://pro-expense-manager.herokuapp.com/expense/recent/');
-        const data = await res.json();
-        setExpenses(data);
-    }
+    const [expenses, setExpenses] = useState<ExpenseProps[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        fetchRecentExpenses();
-    });
+        setIsLoading(true);
+        fetch('https://pro-expense-manager.herokuapp.com/expense/recent/')
+            .then(res => res.json())
+            .then(data => {
+                setExpenses(data);
+                setIsLoading(false);
+            })
+    }, []);
 
-    if (error) {
-        return <div>Error: {error}</div>
+    if (isLoading) {
+        return <Layout>
+            <div className={"text-xl"}>
+                Loading...
+            </div>
+        </Layout>
     }
 
     return (
